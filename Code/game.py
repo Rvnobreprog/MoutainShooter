745482cf528b75b60e3f499cdfe2c984518dd790
Ronaldprog <rvnobreprog@gmail.com>
2025-05-02 14:26:37 -0300

    NOVO MAIN.PY

--- Code/game.py
+++ Code/game.py
@@ -3,15 +3,18 @@
 
 import pygame
 
+from Code.Const import WIN_WIDTH, WIN_WEIGHT
 from Code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_WEIGHT))
 
-    def run(self, ):
+    def run(self):
+
+
         while True:
             menu = Menu(self.window)
             menu.run()
