2be175cbec82b69483ddf1e84e6e980b0b527498
Ronaldprog <rvnobreprog@gmail.com>
2025-05-28 10:41:13 -0300

    Hit box Collisions HUD Score

--- Code/Entity.py
+++ Code/Entity.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 
 import pygame.image
 
-from Code.Const import ENTITY_HEALTH
+from Code.Const import ENTITY_HEALTH, ENTITY_DAMAGE, ENTITY_SCORE
 
 
 class Entity(ABC):
@@ -14,7 +14,10 @@ class Entity(ABC):
         self.surf = pygame.image.load('../Assets/' + name+ '.png').convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
-        self.heath = ENTITY_HEALTH[self.name]
+        self.health = ENTITY_HEALTH[self.name]
+        self.damage = ENTITY_DAMAGE[self.name]
+        self.score = ENTITY_SCORE[self.name]
+        self.last_dmg = 'None'
 
 
     @abstractmethod
